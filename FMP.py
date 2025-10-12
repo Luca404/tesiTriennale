efa4cbcfb4f5ee8a10fc8cfed3142bb728fa103b
Luca404 <luca.botta44@gmail.com>
2025-10-12 14:35:45 +0200

    sistemato get_trends, iniziato get_news_weekly_counts in FMP

--- FMP.py
+++ FMP.py
@@ -8,20 +8,19 @@ import os
 
 #salvo percorsi utili
 PATH = Path(__file__).parent
-INDEX_PATH = PATH/".."/"indexes"
 
 #carico api key
 load_dotenv( PATH / "keys.env" )
 API_KEY = os.getenv( "API_KEY_FMP_PREMIUM" )
 
 #scarico market cap giornaliera di tutti i (tickers)
-def get_mkt_cap( tickers, pause ):
-    output_name = DATA_PATH/"mktcap_D.csv"
+def get_mkt_cap( tickers, pause, output_name="all_mktcap_D.csv" ):
+    output_path = DATA_PATH/output_name
 
     #carico tickers già scaricati
     old_tickers = []
-    if output_name.is_file():
-        existing_data = pd.read_csv(output_name)
+    if output_path.is_file():
+        existing_data = pd.read_csv(output_path)
         old_tickers = existing_data["ticker"].dropna().unique().tolist()
     
     remaining_tickers = [t for t in tickers if t not in old_tickers]    #prendo tutti i ticker dopo quelli già scaricati
@@ -54,21 +53,21 @@ def get_mkt_cap( tickers, pause ):
     if mkt_cap:
         final_df = pd.concat(mkt_cap, ignore_index=True)
         if old_tickers:
-            final_df.to_csv(output_name, mode='a', header=False, index=False)
+            final_df.to_csv(output_path, mode='a', header=False, index=False)
         else:
-            final_df.to_csv(output_name, index=False)
+            final_df.to_csv(output_path, index=False)
         print(f"Dati salvati correttamente")
     else:
         print("Nessun dato da salvare.")
 
 #scarico prezzi giornalieri di tutti i (tickers)
-def get_prices( tickers, pause ):
-    output_name = DATA_PATH/"all_prices_D.csv"
+def get_prices( tickers, pause, output_name="all_prices_D.csv" ):
+    output_path = DATA_PATH/output_name
 
     #carico tickers già scaricati
     old_tickers = []
-    if output_name.is_file():
-        existing_data = pd.read_csv(output_name)
+    if output_path.is_file():
+        existing_data = pd.read_csv(output_path)
         old_tickers = existing_data["ticker"].dropna().unique().tolist()
     
     remaining_tickers = [t for t in tickers if t not in old_tickers]    #prendo tutti i ticker dopo quelli già scaricati
@@ -100,17 +99,81 @@ def get_prices( tickers, pause ):
     if prices:
         final_df = pd.concat(prices, ignore_index=True)
         if old_tickers:
-            final_df.to_csv(output_name, mode='a', header=False, index=False)
+            final_df.to_csv(output_path, mode='a', header=False, index=False)
         else:
-            final_df.to_csv(output_name, index=False)
+            final_df.to_csv(output_path, index=False)
         print(f"Dati salvati correttamente")
     else:
         print("Nessun dato da salvare.")
-    
+
+
+def get_news_weekly_counts(tickers, pause, start_date="2020-01-01", output_name="all_news_volume.csv"):
+   
+    output_path = DATA_PATH / output_name
+
+    #carico tickers già scaricati
+    old_tickers = []
+    if output_path.is_file():
+        existing_data = pd.read_csv(output_path)
+        old_tickers = existing_data["ticker"].dropna().unique().tolist()
+
+    remaining_tickers = [t for t in tickers if t not in old_tickers]
+
+    #finestra temporale
+    start = pd.to_datetime(start_date).strftime("%Y-%m-%d")
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    for ticker in remaining_tickers:
+        api_ticker = ticker.replace(".", "-") #sostituisco eventuali punti
+        url = f"{BASE_URL}/stock_news"
+
+        #scorro tutte le pagine
+        page = 0
+        while(True):
+            params = {
+                "tickers": api_ticker,
+                "from": start,
+                "to": today,
+                "page": page,
+                "apikey": API_KEY
+            }
+            try:
+                r = requests.get(url, params)
+                r.raise_for_status()
+                data = r.json()
+                if not data:  # se non ci sono dati, skip
+                    print(f"Nessun dato per {ticker}")
+                    continue
+                df = pd.DataFrame(data)
+                df["publishedDate"] = pd.to_datetime(df["publishedDate"])
+                weekly = (
+                    df.set_index("publishedDate")
+                    .resample("W-SUN")
+                    .size()
+                    .rename("news_count")
+                    .reset_index()
+                    .rename(columns={"publishedDate": "date"})
+                )
+
+                print(df)
+                print(weekly)
+
+                time.sleep(1000)
+
+            except Exception as e:
+                print(f"Errore per {ticker}: {e}")
+        
+        time.sleep(pause)
+
+
+    print(f"Dati news settimanali aggiornati in {output_path}.")
+    return remaining_tickers
+
+
 
 INDEX = "R3000"
 YEAR = "23-24"
-DATA_PATH = PATH/f"data{INDEX}"
+DATA_PATH = PATH/"data"/f"data{INDEX}"
 
 BASE_URL = "https://financialmodelingprep.com/api/v3"
 
@@ -123,5 +186,7 @@ if __name__ == "__main__":
 
     #get_mkt_cap( tickers, pause )
 
-    get_prices( tickers, pause )
+    #get_prices( tickers, pause )
+
+    get_news_weekly_counts( tickers, pause )
 
--- get_trends.py
+++ get_trends.py
@@ -6,10 +6,19 @@ import warnings
 warnings.simplefilter("ignore", FutureWarning)
 
 
-def get_google_trends(tickers, tf):
+def get_google_trends(tickers, tf, output_name="all_trends_W.csv"):
+    output_path = DATA_PATH / output_name
+    #carico tickers già scaricati
+    old_tickers = []
+    if output_path.is_file():
+        existing_data = pd.read_csv(output_path)
+        old_tickers = existing_data.columns.tolist()
+    
+    remaining_tickers = [t for t in tickers if t not in old_tickers]    #prendo tutti i ticker dopo quelli già scaricati
+
     pytrends = TrendReq(hl='en-US', tz=360)
     data = []
-    for ticker in tickers:
+    for ticker in remaining_tickers:
         try:
             #richiesta, cat=categoria, geo=stato, gprop=tipo (news, youtube, images, ...)
             pytrends.build_payload([ticker], cat=0, timeframe=tf, geo="US", gprop="")
@@ -19,16 +28,20 @@ def get_google_trends(tickers, tf):
                 print( f"Scaricato {ticker}" )
                 interest = interest[[ticker]]
                 data.append(interest)
-            
-            time.sleep(1)  #rispetta i limiti di Google
 
         except Exception as e:
             print(f"Errore con {ticker}: {e}")
+        
+        time.sleep(5)  #rispetta i limiti di Google
 
     #se ci sono dati concateno e salvo
     if data:
         df = pd.concat(data, axis=1)
-        df.to_csv(DATA_PATH/"all_trends.csv")
+        if old_tickers:
+            df.to_csv(output_path, mode="a", header=False, index=False )
+        else:
+            df.to_csv(output_path, index=False )
+
         print("Salvataggio completato")
     else:
         print("Nessun dato disponibile")
